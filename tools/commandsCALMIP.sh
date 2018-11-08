2f3c0e068df32907e5fa7f0948418be4e6ffa5e0
inakiFernandez <inaki.fernandez@loria.fr>
2018-11-08 15:07:45 +0100

    Calmip tests

--- tools/commandsCALMIP.sh
+++ tools/commandsCALMIP.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #SBATCH -J script_art633
+#SBATCH --job-name=test_emb_arr
+#SBATCH --output=res_emb_arr.txt
+#
 #SBATCH -N 4
 #SBATCH -n 128
-#SBATCH --ntasks-per-node=36
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=32
 #SBATCH --ntasks-per-core=1
 #SBATCH --time=00:03:00
 #SBATCH --mail-user=fernandezperez.inaki@gmail.com
 #SBATCH --array=1-256
+#SBATCH --mem-per-cpu=100
+
 srun ./commandeALaCon.sh $SLURM_ARRAY_TASK_ID
 
