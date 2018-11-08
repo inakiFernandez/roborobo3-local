1be316eb8d80916c25b8c681bc0c63657a978cdc
inakiFernandez <inaki.fernandez@loria.fr>
2018-11-08 16:14:53 +0100

    Calmiptests

--- tools/commandsCALMIP.sh
+++ tools/commandsCALMIP.sh
@@ -3,13 +3,12 @@
 #SBATCH --job-name=test_emb_arr
 #SBATCH --output=res_emb_arr.txt
 #
-#SBATCH -N 4
+#SBATCH -N 5
 #SBATCH -n 20
-#SBATCH --time=00:03:00
 #SBATCH --mail-user=fernandezperez.inaki@gmail.com
 #SBATCH --mail-type=ALL
 
 
-parallel -j80 -a ./commandeALaCon.sh
+parallel -j80 -a ./config/evolvability.properties.parallel
 #$SLURM_JOB_ID
 
